1acd1abd4d66c74cd632be91ae6b53598f03bb57
Diamondduke <reza.sarmast@brightstraining.com>
2024-01-17 15:46:44 +0100

    Lagt til Migration og tilhørende Database

--- PitchMatch/Data/Models/Investment.cs
+++ PitchMatch/Data/Models/Investment.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         public decimal Amount { get; set; }
-        public int PitchId { get; set; }
-        public int UserId { get; set; }
+        public int? PitchId { get; set; }
+        public int? UserId { get; set; }
         public Pitch? Pitch { get; set; }
         public User? User { get; set; }
     }
---
+++ PitchMatch/Migrations/20240117144510_intial.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PitchMatch.Migrations
+{
+    /// <inheritdoc />
+    public partial class intial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salt = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Contact = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SoMe = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImgUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonalData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonNr = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsVerified = table.Column<bool>(type: "bit", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonalData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PersonalData_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pitch",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImgUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Goal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Funding = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Views = table.Column<int>(type: "int", nullable: false),
+                    Categories = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pitch", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pitch_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Investment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PitchId = table.Column<int>(type: "int", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Investment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Investment_Pitch_PitchId",
+                        column: x => x.PitchId,
+                        principalTable: "Pitch",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Investment_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Investment_PitchId",
+                table: "Investment",
+                column: "PitchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Investment_UserId",
+                table: "Investment",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonalData_UserId",
+                table: "PersonalData",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pitch_UserId",
+                table: "Pitch",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Investment");
+
+            migrationBuilder.DropTable(
+                name: "PersonalData");
+
+            migrationBuilder.DropTable(
+                name: "Pitch");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
