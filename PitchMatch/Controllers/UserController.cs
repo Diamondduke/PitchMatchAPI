1fa39536df5ad6b0097949f4af8f296df1aed6d3
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-01-18 11:35:45 +0100

    Added UserController with Read and Create user

---
+++ PitchMatch/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PitchMatch.Data;
+using PitchMatch.Data.Models;
+
+namespace PitchMatch.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly PitchMatchDbContext _db;
+
+        public UserController(PitchMatchDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int userId)
+        {
+            var user= await _db.User.FindAsync(userId);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(User user)
+        {
+            var newUser = new User
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Password = user.Password,
+                Bio=user.Bio,
+                SoMe=user.SoMe,
+                ImgUrl=user.ImgUrl,
+                Contact=user.Contact,
+            };
+
+            _db.User.Add(newUser);
+            await _db.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new {id=newUser.Id},newUser);
+        }
+
+    }
+}
