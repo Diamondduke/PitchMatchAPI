d18730c67514c8db8ebff69b6903550ca6be1db3
Renatuscape <silarcta@gmail.com>
2024-01-19 15:26:29 +0100

    Error messages added to User table.

--- PitchMatch/Data/Models/User.cs
+++ PitchMatch/Data/Models/User.cs
@@ -6,27 +6,27 @@ namespace PitchMatch.Data.Models
     {
         public int Id { get; set; }
 
-        [Required]
-        [MinLength(3)]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "A name is required.")]
+        [MinLength(3, ErrorMessage = "Name must be between 3 and 100 characters.")]
+        [MaxLength(100, ErrorMessage = "Name must be between 3 and 100 characters.")]
         public string Name { get; set; } = string.Empty;
-        [Required]
-        [MinLength(3)]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "A unique email is required.")]
+        [MinLength(3, ErrorMessage = "Email must be between 3 and 100 characters.")]
+        [MaxLength(100, ErrorMessage = "Email must be between 3 and 100 characters.")]
         public string Email { get; set; } = string.Empty;
-        [Required]
-        [MinLength(6)]
-        [MaxLength(300)]
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least six characters.")]
+        [MaxLength(300, ErrorMessage = "We don't have space for that much password!")]
         public string Password { get; set; } = string.Empty;
-        [MaxLength(2000)]
+        [MaxLength(2000, ErrorMessage = "A biography can be 2000 characters at most. A short and snappy introduction works best!")]
         public string? Bio { get; set; }
-        [MaxLength(300)]
+        [MaxLength(300, ErrorMessage = "We don't have space for that much information! 300 character limit.")]
         public string? Contact { get; set; }
-        [MaxLength(300)]
+        [MaxLength(300, ErrorMessage = "We don't have space for that much information! 300 character limit.")]
         public string? SoMe { get; set; }
-        [MaxLength(300)]
+        [MaxLength(300, ErrorMessage = "We don't have space for that much information! 300 character limit.")]
         public string? ImgUrl { get; set; }
-        [MaxLength(300)]
+        [MaxLength(300, ErrorMessage = "We don't have space for that much information! 300 character limit.")]
         public string? CvUrl { get; set; }
         public string Salt { get; set; } = string.Empty;
         public string Location { get; set; } = string.Empty;
---
+++ PitchMatch/Migrations/20240119142545_UserTableErrorMessagesAdded.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PitchMatch.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserTableErrorMessagesAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
