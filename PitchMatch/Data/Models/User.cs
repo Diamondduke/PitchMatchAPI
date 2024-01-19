6795a62068202e4e8a323cdf5761bccf9f1d09a9
Renatuscape <silarcta@gmail.com>
2024-01-19 15:16:14 +0100

    Added input validation to User table

--- PitchMatch/Data/Models/User.cs
+++ PitchMatch/Data/Models/User.cs
@@ -5,17 +5,32 @@ namespace PitchMatch.Data.Models
     public class User
     {
         public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
         public string Name { get; set; } = string.Empty;
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
         public string Email { get; set; } = string.Empty;
+        [Required]
+        [MinLength(6)]
+        [MaxLength(300)]
         public string Password { get; set; } = string.Empty;
-        public string Salt { get; set; } = string.Empty;
-        public string? Bio {  get; set; }
+        [MaxLength(2000)]
+        public string? Bio { get; set; }
+        [MaxLength(300)]
         public string? Contact { get; set; }
+        [MaxLength(300)]
         public string? SoMe { get; set; }
+        [MaxLength(300)]
         public string? ImgUrl { get; set; }
+        [MaxLength(300)]
+        public string? CvUrl { get; set; }
+        public string Salt { get; set; } = string.Empty;
         public string Location { get; set; } = string.Empty;
         public int Rating { get; set; }
-        public string? CvUrl { get; set; }
         public PersonalData? PersonalData { get; set; }
         public ICollection<Pitch>? Pitches { get; set; }
         public ICollection<Investment>? Investments { get; set; }
