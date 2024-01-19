8ad6849d2c1318eb838040dfba44e085737a9126
Renatuscape <silarcta@gmail.com>
2024-01-19 15:49:31 +0100

    Added error messages to user controller classes.

--- PitchMatch/Controllers/UserController.cs
+++ PitchMatch/Controllers/UserController.cs
@@ -104,13 +104,27 @@ namespace PitchMatch.Controllers
 
     public class CreateUser
     {
+        [Required(ErrorMessage = "A name is required.")]
+        [MinLength(3, ErrorMessage = "Name must be between 3 and 100 characters.")]
+        [MaxLength(100, ErrorMessage = "Name must be between 3 and 100 characters.")]
         public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "A unique email is required.")]
+        [MinLength(3, ErrorMessage = "Email must be between 3 and 100 characters.")]
+        [MaxLength(100, ErrorMessage = "Email must be between 3 and 100 characters.")]
         public string Email { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least six characters.")]
+        [MaxLength(300, ErrorMessage = "We don't have space for that much password!")]
         public string Password { get; set; } = string.Empty;
+        [MaxLength(2000, ErrorMessage = "A biography can be 2000 characters at most. A short and snappy introduction works best!")]
         public string? Bio { get; set; }
+        [MaxLength(300, ErrorMessage = "We don't have space for that much information! 300 character limit.")]
         public string? Contact { get; set; }
+        [MaxLength(300, ErrorMessage = "We don't have space for that much information! 300 character limit.")]
         public string? SoMe { get; set; }
+        [MaxLength(300, ErrorMessage = "We don't have space for that much information! 300 character limit.")]
         public string? ImgUrl { get; set; }
+        [MaxLength(300, ErrorMessage = "We don't have space for that much information! 300 character limit.")]
         public string? CvUrl { get; set; }
 
     }
@@ -118,12 +132,23 @@ namespace PitchMatch.Controllers
     public class UpdateUser
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "A name is required.")]
+        [MinLength(3, ErrorMessage = "Name must be between 3 and 100 characters.")]
+        [MaxLength(100, ErrorMessage = "Name must be between 3 and 100 characters.")]
         public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least six characters.")]
+        [MaxLength(300, ErrorMessage = "We don't have space for that much password!")]
         public string Password { get; set; } = string.Empty;
+        [MaxLength(2000, ErrorMessage = "A biography can be 2000 characters at most. A short and snappy introduction works best!")]
         public string? Bio { get; set; }
+        [MaxLength(300, ErrorMessage = "We don't have space for that much information! 300 character limit.")]
         public string? Contact { get; set; }
+        [MaxLength(300, ErrorMessage = "We don't have space for that much information! 300 character limit.")]
         public string? SoMe { get; set; }
+        [MaxLength(300, ErrorMessage = "We don't have space for that much information! 300 character limit.")]
         public string? ImgUrl { get; set; }
+        [MaxLength(300, ErrorMessage = "We don't have space for that much information! 300 character limit.")]
         public string? CvUrl { get; set; }
 
     }
