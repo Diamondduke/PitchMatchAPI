6dbfaff05dea4d08b9a7da8f9051ea1ed77a475f
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-01-22 13:45:27 +0100

    Added a HomeController that takes 9 pitches to the home page

---
+++ PitchMatch/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PitchMatch.Data;
+
+
+namespace PitchMatch.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly PitchMatchDbContext _db;
+
+        public HomeController(PitchMatchDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetTopPitches()
+        {
+            // Retrieve the top 6 pitches with the most views
+            var topPitches = _db.Pitch
+                .OrderByDescending(p => p.Views)
+                .Take(9)
+                .ToList();
+
+            return Ok(topPitches);
+        }
+
+    }
+}
