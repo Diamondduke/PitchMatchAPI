fcd366d5ec1b1575751de7a6a9268331c08f69d6
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-01-22 10:56:54 +0100

    Added verification in controller

--- PitchMatch/Controllers/PitchController.cs
+++ PitchMatch/Controllers/PitchController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PitchMatch.Data;
 using PitchMatch.Data.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace PitchMatch.Controllers
 {
@@ -99,14 +100,27 @@ namespace PitchMatch.Controllers
     public class CreatePitch
     {
         public int UserId { get; set; }
+
+        [Required(ErrorMessage = "A title is required.")]
+        [MinLength(3, ErrorMessage = "Title must be between 3 and 100 characters.")]
+        [MaxLength(100, ErrorMessage = "Title must be between 3 and 100 characters.")]
         public string Title { get; set; } = string.Empty;
+        [Required(ErrorMessage = "A summary is required.")]
+        [MaxLength(400, ErrorMessage = "We don't have space for that much information! 400 character limit.")]
         public string Summary { get; set; } = string.Empty;
+        [Required(ErrorMessage = "A description is required.")]
         public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = "A picture is required.")]
         public string? ImgUrl { get; set; }
         public string? VideoUrl { get; set; }
         public string Location { get; set; } = string.Empty;
+        [Required(ErrorMessage = "A goal capital is required")]
+        [Range(0, 10000000000, ErrorMessage = "Goal capital must be between 100 and 10000000000")]
         public decimal Goal { get; set; }
+        [Required(ErrorMessage = "A yield is required")]
+        [Range(0, 10000000000, ErrorMessage = "Yield must be between 100 and 10000000000")]
         public decimal Yield { get; set; }
+        [Required(ErrorMessage = "A category is required")]
         public string Categories { get; set; } = string.Empty;
     }
 }
