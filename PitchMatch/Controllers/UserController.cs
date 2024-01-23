f02efa62f42e8523b7c637925e6af134188d06c4
Renatuscape <silarcta@gmail.com>
2024-01-23 15:20:17 +0100

    Undid user get by id changes because of json depth error

--- PitchMatch/Controllers/UserController.cs
+++ PitchMatch/Controllers/UserController.cs
@@ -30,7 +30,7 @@ namespace PitchMatch.Controllers
         [HttpGet("{id:int}")]
         public async Task<IActionResult> GetUser(int id)
         {
-            var user = await _db.User.Where(u => u.Id == id).Include(u => u.Pitches).FirstOrDefaultAsync();
+            var user = await _db.User.FindAsync(id);
             if(user == null)
             {
                 return NotFound();
