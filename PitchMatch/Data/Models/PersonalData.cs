1ea154219dab0bf4ae330c15e8968ca7d431a6ad
Renatuscape <silarcta@gmail.com>
2024-01-23 09:07:10 +0100

    Included coordinates in personal data

--- PitchMatch/Data/Models/PersonalData.cs
+++ PitchMatch/Data/Models/PersonalData.cs
@@ -8,6 +8,8 @@ namespace PitchMatch.Data.Models
         public string? PhoneNumber { get; set; }
         public string? PersonNr { get; set; }
         public string? Address { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
         public bool IsVerified { get; set; }
         public int UserId { get; set; }
         public User? User { get; set; }
