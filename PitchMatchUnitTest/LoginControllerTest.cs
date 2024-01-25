80c34a6d340332c2143ec23b43cf6b2d3a8bd280
Diamondduke <reza.sarmast@brightstraining.com>
2024-01-25 16:24:59 +0100

    Added XUnit test

---
+++ PitchMatchUnitTest/LoginControllerTest.cs
@@ -0,0 +1,16 @@
+namespace PitchMatchUnitTest
+{
+    public class LoginControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
